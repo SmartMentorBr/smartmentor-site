08604e4dcc6378ec534e35403e4fffca3d642dfc
Walter Melo <41174751+walmello@users.noreply.github.com>
2024-01-03 19:16:49 -0300

    downgrading to 13.4

--- components/Cookies.jsx
+++ components/Cookies.jsx
@@ -15,10 +15,10 @@ export default function(){
         setCookies(localStorage.getItem("cookies"))
     },[])
 
-    return cookies ? <></> : <div className="fixed bottom-0 w-full bg-black text-white">
+    return cookies ? <></> : <div className="fixed bottom-0 w-full bg-black text-white z-10">
         <Container>
-            <div>
-                <div className="">
+            <div className="lg:flex justify-between items-center">
+                <div className="flex flex-col gap-0.5 max-w-1x">
                     <p className="font-bold">
                         Este Site usa Cookies 
                     </p>
