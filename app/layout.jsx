859da91dacf49a6471912aa939a8363a45a07f53
Walter Melo <41174751+walmello@users.noreply.github.com>
2024-01-07 03:42:51 -0300

    adding tag manager component

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,8 @@ import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Navigation from '@/components/sections/navigation/Navigation'
 import Footer from '@/components/sections/Footer'
-import useData from '@/hooks/useData'
 import SmoothScroll from '@/components/effects/SmoothScroll'
-import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const font = Montserrat({ subsets: ['latin'] })
 
@@ -12,39 +11,23 @@ export const metadata = {
   charset: 'utf-8',
   title: 'Smartmentor',
   description: 'Descrição do site para fins de SEO aqui',
-  viewport: 'width=device-width, initial-scale=1.0, minimum-scale=1',  
+}
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <head>
-        <link rel="icon" href="images/favicon.png"/>
-      </head>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `<!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5HR6W9Q');</script>
-            <!-- End Google Tag Manager -->`,
-          }}
-        />
-      </Head>
       <body className={font.className}>
-        <script dangerouslySetInnerHTML={{
-          __html: `<!-- Google Tag Manager (noscript) -->
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5HR6W9Q"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          <!-- End Google Tag Manager (noscript) -->`
-        }}/>
         <Navigation/>
         {children}
         <Footer/>
         <SmoothScroll/>
+        <GoogleTagManager gtmId="GTM-5HR6W9Q" />
       </body>
     </html>
   )
